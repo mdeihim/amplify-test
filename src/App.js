b6103f04829f1740d49d63677ad07da24079ff69
Mourad Deihim <mourad.deihim@gmail.com>
2023-02-17 13:10:22 -0500

    sal update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sal loves logan paul
         </a>
       </header>
     </div>
